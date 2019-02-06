29f5df14ac4a304df0c44115d65d2b5657be9e42
beviga1999 <as3-2-21.18@uni.eus>
2019-02-06 17:31:18 +0100

    .

--- login.php
+++ login.php
@@ -2,8 +2,13 @@
 
 session_start();
 
+require_once 'vendor/autoload.php';
+$loader = new Twig_Loader_Filesystem('Templates');
+$twig = new Twig_Environment($loader);
+
 $user = $_POST["user"];
 $passwd = $_POST["pass"];
+
 // connect to mySQL
 $servername = "localhost";
 $username = "root";
@@ -11,29 +16,36 @@ $password = "";
 $database = "web";
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
-if (!$conn) {
+/*if (!$conn) {
     die("Connection failed: ". mysqli_connect_error());
 }
 else{
-echo "Connected succesfully"."<br>";}
+echo "Connected succesfully"."<br>";}*/
 
 //select from user where
 $query = "SELECT pass FROM usuarios WHERE user = '$user'";
 $result = mysqli_query($conn, $query);
 //var_dump($result);
 $row = mysqli_fetch_array($result, MYSQLI_NUM);
-$hash = $row[0];
-if (password_verify($passwd, $hash)){
+//$hash = $row[0];
+if ($row && password_verify($passwd, $row[0])){
    
     $_SESSION['usuario'] = $user;
-    $_SESSION['error'] = 0;
+    
 
-    header("Location: ../index.html");
+    //header("Location: ../index.html");
+
+    //echo $twig->render('login.html', ['error' => $error ] );
+
+    echo $twig->render('index.html');
 
 } else {
 
-    $_SESSION['error'] = 1;
-    echo header('Location: '.'../prueba.php');
+    
+    //header('Location: '.'../prueba.php');
 
+    echo $twig->render('login.html', ['error' => 1] );
+
+   //echo $twig->render('login.html');
 }
 ?>
---
+++ render_login.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+require_once 'vendor/autoload.php';
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+
+$error = 0;
+if (isset($_SESSION['user'])) {
+    
+    echo $twig->render('index.html');
+} else{    
+    echo $twig->render('login.html', ['error' => $error] );
+}
